73c00112c74d1d4bb2a27acfe02046f9a7510960
Frederick Wong <frederick.kf.wong@gmail.com>
2014-12-27 00:01:53 +1100

    Multiple markers working now

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,44 +1,48 @@
 class BikesController < ApplicationController
-  def index
-  	@bikes = Bike.all
-  end
+	def index
+		@bikes = Bike.all
+		@hash = Gmaps4rails.build_markers(@bikes) do |bike, marker|
+			marker.lat bike.latitude
+			marker.lng bike.longitude
+	end
+	end
 
-  def new
-  	@bike = Bike.new
-  end
+	def new
+		@bike = Bike.new
+	end
 
-  def create
-  	@bike = Bike.new bike_params
-  	if @bike.save
-  		redirect_to bikes_path
-  	else
-  		render :new
-  	end
-  end
+	def create
+		@bike = Bike.new bike_params
+		if @bike.save
+			redirect_to bikes_path
+		else
+			render :new
+		end
+	end
 
-  def edit
-  	@bike = Bike.find params[:id]
-  end
+	def edit
+		@bike = Bike.find params[:id]
+	end
 
-  def update
-  	@bike = Bike.find params[:id]
-  	if @bike.update bike_params
-  		redirect_to bikes_path
-  	else
-  		render :edit
-  	end
-  end
+	def update
+		@bike = Bike.find params[:id]
+		if @bike.update bike_params
+			redirect_to bikes_path
+		else
+			render :edit
+		end
+	end
 
-  def destroy
-  	@bike = Bike.find params[:id]
-  	@bike.destroy
-  	redirect_to bikes_path	
-  end
+	def destroy
+		@bike = Bike.find params[:id]
+		@bike.destroy
+		redirect_to bikes_path	
+	end
 
-  private
+	private
 
-  def bike_params
-  	params.require(:bike).permit(:brand, :category, :description, :year_made)
-  end
+	def bike_params
+		params.require(:bike).permit(:brand, :category, :description, :year_made, :latitude, :longitude, :address)
+	end
 
 end
